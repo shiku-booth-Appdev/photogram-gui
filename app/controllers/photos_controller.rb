77f1f873674cc72a2db63c149473243e2792d80a
shiku-booth <sbarathi@chicagobooth.edu>
2021-11-13 15:26:14 +0000

    delete complete

---
+++ app/controllers/photos_controller.rb
@@ -0,0 +1,38 @@
+class PhotosController < ApplicationController
+
+  def index
+    matching_photos = Photo.all
+
+    @list_of_photos = matching_photos.order({ :created_at => :desc })
+
+    render({ :template => "photo_templates/index.html.erb"})
+  end
+
+  def show
+    url_photo_id = params.fetch("path_photo_id")
+
+    matching_photos = Photo.where({ :id => url_photo_id})
+
+    @the_photo = matching_photos.first
+
+    if @the_photo == nil
+     redirect_to("/404")
+    else
+     render({ :template => "photo_templates/show.html.erb"})
+    end
+  end
+
+  def delete_photo
+    url_delete_photo_id = params.fetch("path_delete_photo_id")
+
+    matching_photos = Photo.where({ :id => url_delete_photo_id})
+
+    the_photo = matching_photos.first
+
+    the_photo.destroy
+
+    redirect_to("/photos")
+
+  end
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -4,4 +4,10 @@ Rails.application.routes.draw do
 
  get("/users/:path_username", { :controller => "users", :action => "show"})
 
+ get("/photos", { :controller => "photos", :action => "index"})
+
+ get("/photos/:path_photo_id", { :controller => "photos", :action => "show"})
+
+ get("/delete_photo/:path_delete_photo_id", { :controller => "photos", :action => "delete_photo"})
+
 end
